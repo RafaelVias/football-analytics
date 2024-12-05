b5e8c42adaedb1dc0bf20cad79cd7d185cacf00a
Rafael Daníel Vias <78622738+RafaelVias@users.noreply.github.com>
2024-12-05 20:30:51 +0000

    looking at data

--- R/data_acquisition.R
+++ R/data_acquisition.R
@@ -10,13 +10,21 @@ Comps <- FreeCompetitions()
 # Filter for desired competitions
 # Modify these filters as needed for different competitions/seasons
 Comps <- Comps |>
-    filter(competition_name == "UEFA Euro",
-           season_name == "2024")
+    filter(competition_name == "La Liga",
+           season_name == "2020/2021")
 
 # Get matches and events data
 Matches <- FreeMatches(Comps)
 StatsBombData <- free_allevents(MatchesDF = Matches, Parallel = T)
 StatsBombData <- allclean(StatsBombData)
+
+# totals
+data <- StatsBombData |>
+    group_by(match_id,team.name, competition_id, season_id) |>
+    summarise(shots = sum(type.name == "Shot", na.rm = T),
+              goals = sum(shot.outcome.name == "Goal", na.rm = T),
+              xg = sum(shot.statsbomb_xg, na.rm = T)) |>
+    mutate(goals_to_shots = goals / shots)
 
 # Save processed data
 # Create 'data' directory if it doesn't exist
